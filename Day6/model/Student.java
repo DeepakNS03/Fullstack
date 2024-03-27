3d08c71f27c103a8de10da318e56b28a862d73cf
Deepak NS <126231114+DeepakNS03@users.noreply.github.com>
2024-03-27 10:23:36 +0530

    Add files via upload

---
+++ Day6/model/Student.java
@@ -0,0 +1,32 @@
+package com.java.zenyoga.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="dp_student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String studentId;
+
+    
+    private String studentName;    
+    private String studentDOB;  
+    private String address;
+    private String mobile;
+    private String age;
+    
+}
